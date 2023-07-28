19f37e9be8c8e56c28d65e91b7274d45de2f7c77
Wouter Schoenmakers <w.k.schoenmakers@gmail.com>
2023-07-28 17:08:29 +0200

    write

--- app.py
+++ app.py
@@ -53,7 +53,9 @@ if isinstance(gisib,gpd.GeoDataFrame) and isinstance(pakket,gpd.GeoDataFrame):
         guids_correct = True
         st.write("Guids ✅")
     check_result = result.loc[lambda df: ((df.overlap_percentage > 99.9) & (df.oppervlak_percentage > 99.9))]
-    st.write(result.loc[lambda df: ~((df.overlap_percentage > 99.9) & (df.oppervlak_percentage > 99.9)),["Guid_right","overlap_percentage","oppervlak_percentage"]])
+    st.write("Dit is de Guid die wel in het pakket voorkomt maar niet een 100% match heeft")
+    st.write(set(pakket.Guid) - set(check_result.Guid_right))
+    st.write(check_result.loc[lambda df: df.Guid_right.isin(list(set(pakket.Guid) - set(check_result.Guid_right))),["overlap_percentage","oppervlak_percentage"]])
     # st.write(f"Er zijn {check_result.shape[0]} vlakken die een overlap en oppervlak percentage > 99.9 hebben")
     results["Guid matches"] = len(guid_check)
     results["Geometry matches"] = check_result.shape[0]
